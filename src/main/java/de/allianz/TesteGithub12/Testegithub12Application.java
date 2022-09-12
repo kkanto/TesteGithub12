109b310feb486d27f75b9c88c805d73cb0e96a97
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-12 22:07:51 +0200

    Create maven project

---
+++ src/main/java/de/allianz/TesteGithub12/Testegithub12Application.java
@@ -0,0 +1,13 @@
+package de.allianz.TesteGithub12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testegithub12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testegithub12Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/TesteGithub12/Testegithub12ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.TesteGithub12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testegithub12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
